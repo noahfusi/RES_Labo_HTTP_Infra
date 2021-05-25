537f5ee3b180de20c9041e45a79fea7b3185788f
Chiffelle <janischiffelle@hotmail.ch>
2021-05-25 15:40:45 +0200

    Added a docker image wrapping an express.js that returns random array of web identities

--- docker-image/express-image/content/index.js
+++ docker-image/express-image/content/index.js
@@ -1,4 +1,36 @@
 var Chance = require('chance');
 var chance = new Chance();
 
-console.log("Bonjour " + chance.email());
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+  res.send(generateWebIdentity())
+})
+
+app.listen(3000, () => {
+  console.log(`Accepting HTTP request on port 3000!`)
+})
+
+function generateWebIdentity() {
+	var numberOfIdentity = chance.integer({
+		min: 1,
+		max: 10
+	});
+	console.log(numberOfIdentity)
+	var identities = [];
+	for (var i = 0; i < numberOfIdentity; i++){
+		var avatar = chance.avatar();
+		var nameColor = chance.color();
+		var email = chance.email();
+		var pseudo = chance.twitter();
+		identities.push({
+			avatar : avatar,
+			nameColor : nameColor,
+			email : email,
+			pseudo : pseudo
+		});
+	}
+	console.log(identities);
+	return identities;
+}
